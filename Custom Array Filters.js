85fc44f85aec77e7327c6c8321f6ed787042244f
Olga Lubsanova <lubsanova07@gmail.com>
2023-02-27 15:24:51 -0600

    first task

---
+++ Custom Array Filters.js
@@ -0,0 +1,47 @@
+//DESCRIPTION:
+//Dave has a lot of data he is required to apply filters to, which are simple enough, but he wants a shorter way of doing so.
+//
+//He wants the following functions to work as expected:
+//
+// even    // [1,2,3,4,5].even() should return [2,4]
+// odd     // [1,2,3,4,5].odd() should return [1,3,5]
+// under   // [1,2,3,4,5].under(4) should return [1,2,3]
+// over    // [1,2,3,4,5].over(4) should return [5]
+// inRange // [1,2,3,4,5].inRange(1,3) should return [1,2,3]
+//
+//They should also work when used together, for example:
+//
+// [1,2,18,19,20,21,22,30,40,50,100].even().inRange(18,30) // should return [18, 20, 22, 30]
+//
+//And finally the filters should only accept integer values from an array, for example:
+//
+// ["a", 1, "b", 300, "x", "q", 63, 122, 181, "z", 0.83, 0.11].even() // should return [300, 122]
+//
+//Note: List with non-numbers will be tested as well.
+
+//SOLUTION:
+
+Array.prototype.even = function(){
+    let newArr = this.filter(el => el % 1 == 0 && typeof el === 'number').filter(el => el % 2 === 0)
+    return newArr
+}
+
+Array.prototype.odd = function(){
+    let newArr = this.filter(el => el % 1 == 0 && typeof el === 'number').filter(el => el % 2)
+    return newArr
+}
+
+Array.prototype.under = function(x){
+    let newArr = this.filter(el => el % 1 == 0 && typeof el === 'number').filter(el => el < x)
+    return newArr
+}
+
+Array.prototype.over = function(x){
+    let newArr = this.filter(el => el % 1 == 0 && typeof el === 'number').filter(el => el > x)
+    return newArr
+}
+
+Array.prototype.inRange = function(min,max){
+    let newArr = this.filter(el => el % 1 == 0 && typeof el === 'number').filter(el => el >= min && el <= max)
+    return newArr
+}
